7b1cad1a4283b3b96b0f899661d1a1da7594a5dd
Ander Fernandez <anderfernandez.ce@gmail.com>
2024-05-18 12:28:22 +0200

    Feat : db config file added

---
+++ backend/src/configs/db.config.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME || 'trisure',
+  port: process.env.DB_PORT || 3306,
+};
+
+export default db;
