7bc54765c750ee72d0e41e55aed57be369ad15c2
Ander Fernandez <anderfernandez.ce@gmail.com>
2024-06-22 13:07:59 +0200

    Feat: adding section to home page

--- frontend/trisure-frontend/src/layout/Home.tsx
+++ frontend/trisure-frontend/src/layout/Home.tsx
@@ -1,10 +1,14 @@
 import FilterPicker from "./components/FilterPicker"
+import { Header } from "./components/common/Header"
+import { ViewFilters } from "./components/entity-filters/ViewFilters"
 
 export const Home = () => {
   return (
     <>
       <div>
-      <FilterPicker className=""/>  
+        <Header />
+        <ViewFilters></ViewFilters>
+      {/* <FilterPicker className=""/>   */}
       </div>
     </>
   )
